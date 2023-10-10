802c922fb6b0d8484eb29e54dc685f42447e2985
Daniel-Horsnell <z5259702@ad.unsw.edu.au>
2023-10-11 10:21:42 +1100

    Smile detector working git add -A./camera.sh

--- build/image_processing/build/lib/image_processing/smile_detector_node.py
+++ build/image_processing/build/lib/image_processing/smile_detector_node.py
@@ -5,21 +5,22 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
 import numpy as np
+import os
 
 class SmileDetectionNode(Node):
     def __init__(self):
         super().__init__('smile_detection_node')
         self.publisher = self.create_publisher(Image, '/image_raw', 10)
         self.cv_bridge = CvBridge()
-        self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-        self.smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+        self.face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
+        self.smile_cascade = cv2.CascadeClassifier('./haarcascade_smile.xml')
         self.counter = 0
+        self.smile_detected = False
 
-    def detect(self, gray, frame):
+    def detect(self, gray):
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
         for (x, y, w, h) in faces:
             roi_gray = gray[y:y + h, x:x + w]
-            roi_color = frame[y:y + h, x:x + w]
             smiles = self.smile_cascade.detectMultiScale(roi_gray, 1.8, 20)
             if len(smiles) > 0:
                 return True
@@ -30,22 +31,21 @@ class SmileDetectionNode(Node):
         while video_capture.isOpened():
             _, frame = video_capture.read()
             cv2.imshow('Video', frame)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            smile = self.detect(gray, frame)
-            
-            if smile:
-                self.counter += 1
-                if self.counter > 10:
-                    # Publish the image to the /image_raw topic
-                    msg = Image()
-                    msg.header = Header()
-                    msg.header.stamp = self.get_clock().now().to_msg()
-                    msg.encoding = 'bgr8'
-                    msg.data = frame.tobytes()
-                    self.publisher.publish(msg)
-                    break
-            if cv2.waitKey(1) & 0xff == ord('q'):
-                break
+            if (self.smile_detected):
+                ros_image = self.cv_bridge.cv2_to_imgmsg(self.current_image, encoding="bgr8")
+                self.publisher.publish(ros_image)
+                
+            else:
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                smile = self.detect(gray)
+                if smile:
+                    self.counter += 1
+                    if self.counter > 10:
+                        # Publish the image to the /image_raw topic
+                        self.current_image = frame
+                        self.smile_detected = True
+                        print("detected smile")
+                        
 
         video_capture.release()
         cv2.destroyAllWindows()
