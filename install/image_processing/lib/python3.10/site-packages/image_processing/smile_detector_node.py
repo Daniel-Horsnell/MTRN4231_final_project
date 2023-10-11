1d53949261d3991cce32cdaedaf42b8346e07cce
Daniel-Horsnell <z5259702@ad.unsw.edu.au>
2023-10-11 11:59:25 +1100

    added a subscription to the command topic.

--- install/image_processing/lib/python3.10/site-packages/image_processing/smile_detector_node.py
+++ install/image_processing/lib/python3.10/site-packages/image_processing/smile_detector_node.py
@@ -1,21 +1,28 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Header
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-import numpy as np
-import os
 
 class SmileDetectionNode(Node):
     def __init__(self):
         super().__init__('smile_detection_node')
         self.publisher = self.create_publisher(Image, '/image_raw', 10)
+        self.subscriber = self.create_subscription(String, '/commands', self.handle_command, 10)
         self.cv_bridge = CvBridge()
         self.face_cascade = cv2.CascadeClassifier('./haarcascade_frontalface_default.xml')
         self.smile_cascade = cv2.CascadeClassifier('./haarcascade_smile.xml')
         self.counter = 0
         self.smile_detected = False
+        self.subscriber
+        self.video_capture = cv2.VideoCapture(0)
+        self.timer = self.create_timer(0.1, self.timer_callback)
+    
+    def handle_command(self, msg):
+        print(msg.data)
+        if (msg.data == 'restart'):
+            self.smile_detected = False
 
     def detect(self, gray):
         faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
@@ -25,11 +32,10 @@ class SmileDetectionNode(Node):
             if len(smiles) > 0:
                 return True
         return False
-
-    def main(self):
-        video_capture = cv2.VideoCapture(0)
-        while video_capture.isOpened():
-            _, frame = video_capture.read()
+    
+    def timer_callback(self):
+        if self.video_capture.isOpened():
+            _, frame = self.video_capture.read()
             cv2.imshow('Video', frame)
             if (self.smile_detected):
                 ros_image = self.cv_bridge.cv2_to_imgmsg(self.current_image, encoding="bgr8")
@@ -45,16 +51,17 @@ class SmileDetectionNode(Node):
                         self.current_image = frame
                         self.smile_detected = True
                         print("detected smile")
-                        
+        
 
-        video_capture.release()
+    def __del__(self):
+        self.video_capture.release()
         cv2.destroyAllWindows()
 
 
 def main(args=None):
     rclpy.init(args=args)
     smile_detection_node = SmileDetectionNode()
-    smile_detection_node.main()
+    rclpy.spin(smile_detection_node)
     rclpy.shutdown()
 
 
